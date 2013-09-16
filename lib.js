33e10b477af4b87b7c9e214b7aeb57400068e6c5
thailand401 <thailand401@yahoo.com>
2013-09-16 17:01:24 +0700

    update

--- lib.js
+++ lib.js
@@ -2,3 +2,6 @@ function save() {
 	window.localStorage.setItem("key", $("#datas").val());
 	$("#debug").val($("#debug").val()+$("#datas").val()+"\n");
 }
+function get() {
+	$("#debug").val($("#debug").val()+"Get Data: "+window.localStorage.getItem("key")+"\n");
+}
